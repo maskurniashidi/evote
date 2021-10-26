a81d7041274fee84ddaba40e73425e84969446ec
maskurniashidi <maskurshidi12@gmail.com>
2021-10-26 20:03:21 +0700

    inisialisasi eventviewpage dan mengubah routing

---
+++ src/views/EventViewPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="event-view-page"></div>
+</template>
+
+<script>
+export default {
+  name: "EventViewPage",
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
