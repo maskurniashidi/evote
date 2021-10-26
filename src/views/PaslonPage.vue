adb976be179f975586b07c2458757fa036636af5
maskurniashidi <maskurshidi12@gmail.com>
2021-10-26 20:34:25 +0700

    inisialisasi paslon page dan mengubah routing

---
+++ src/views/PaslonPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="paslon-container"></div>
+</template>
+
+<script>
+export default {
+  name: "PaslonPage",
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
